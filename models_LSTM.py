bdeda3e48f043ed907c1c37cd5d23c14c68a09a3
María Victoria <56141354+mvalenciaar@users.noreply.github.com>
2023-05-20 14:21:00 -0500

    :sparkles: change into models_LSTM.py

--- models_LSTM.py
+++ models_LSTM.py
@@ -6,12 +6,26 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
 from keras.layers import Dense,Dropout
 
+data_clean = process_data()
+
 
 # Build the model
-model_lstm = tf.keras.Sequential ([
-    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
-    tf.keras.layers.SpatialDropout1D(0.4),
-    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(lstm_dim)),
-    tf.keras.layers.Dense(dense_dim, activation='relu'),
-    tf.keras.layers.Dense(1, activation='sigmoid')
-])
+with tf.device('/GPU:0'):
+    model_lstm = Sequential ([
+            Embedding(len(embedding_matrix),100, weights = [embedding_matrix], input_length = max_len,trainable=False),
+            Bidirectional(GRU(100, return_sequences = True, dropout = 0.35, recurrent_dropout = 0.35)),
+            Bidirectional(LSTM(100, return_sequences = True, dropout = 0.35, recurrent_dropout = 0.35)),
+            Conv1D(100, 5, activation = 'relu'),
+            GlobalMaxPooling1D(),
+            Dense(100, activation = 'relu'),
+            Dropout(0.5),
+            Dense(1, activation = 'sigmoid')
+        ])
+
+    # Set the training parameters
+    optimizer = Adam(learning_rate=0.001)
+    model_lstm.compile(loss='binary_crossentropy',optimizer=optimizer,metrics=['accuracy'])
+
+model_lstm.summary()
+tf.keras.utils.plot_model(model_lstm,show_shapes=True)
+
